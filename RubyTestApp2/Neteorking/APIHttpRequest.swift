bc9a7b2f68a3faad92a3d2d84ee3dcd756a4ce49
Kazuya Ito <tamaco601@gmail.com>
2020-03-24 07:29:46 -0700

    "first commit"

---
+++ RubyTestApp2/Neteorking/APIHttpRequest.swift
@@ -0,0 +1,44 @@
+//
+//  APIHttpRequest.swift
+//  RubyTestApp2
+//
+
+//
+//  Copyright © 2020 hoya. All rights reserved.
+//
+import Foundation
+import Alamofire
+import Keys
+
+/// レスポンスデータ
+struct ResponseData: Codable {
+    var request_id = ""
+    var output_type = ""
+    var converted = ""
+}
+class APIHttpRequest {
+    
+    init(sentence: String, completion: @escaping (ResponseData) -> Void) {
+        // リクエスト情報
+        let requestUrl = "https://labs.goo.ne.jp/api/hiragana"
+        let appKey = TestAppKeys()
+        let parameters:[String: Any] = [
+            "app_id": appKey.apiTokenRubyApp,
+            "sentence": sentence,
+            "output_type": "hiragana"
+        ]
+        // リクエスト送信
+        AF.request(requestUrl, method:.post, parameters: parameters).responseJSON {
+            
+            response in
+            guard let jsonData = response.data else {
+                return
+            }
+            
+            let responseData = try! JSONDecoder().decode(ResponseData.self, from: jsonData)
+            completion(responseData)
+        }
+        
+    }
+    
+}
--- RubyTestApp2/ViewController.swift
+++ RubyTestApp2/ViewController.swift
@@ -2,19 +2,45 @@
 //  ViewController.swift
 //  RubyTestApp2
 //
-//  Created by Kazuya Ito on 2020/03/24.
+//
 //  Copyright © 2020 hoya. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var humImage: UIImageView!
+    @IBOutlet weak var outputRuby: UILabel!
+    @IBOutlet weak var outputCharacter: UILabel!
+    @IBOutlet weak var inputBox: UITextField!
+    @IBOutlet weak var outputView: UIView!
+    @IBOutlet weak var SendButton: UIButton!
+    @IBOutlet weak var prevCharacter: UILabel!
+    @IBOutlet weak var nextCharacter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    /// ルビ変換ボタン押下時、ルビを画面に表示するメソッド
+    ///
+    /// - Parameter sender: UIButton
+    @IBAction func convertRuby(_ sender: UIButton) {
+        guard let inputText = inputBox.text else {
+            return
+        }
+        
+        if inputText != "" {
+            _ = APIHttpRequest.init(sentence: inputText, completion: {(responseData: ResponseData) -> Void in
+                
+                self.prevCharacter.text = ""
+                self.nextCharacter.text = "って、よむんだよ"
+                
+                self.outputRuby.text = inputText
+                self.outputCharacter.text = responseData.converted
+                
+            })
+        }
+    }
 }
-
