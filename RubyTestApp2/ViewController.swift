de919ac45c6d4e4ebafc882de8b837f336576ed0
Kazuya Ito <tamaco601@gmail.com>
2020-03-24 06:57:31 -0700

    Initial Commit

---
+++ RubyTestApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RubyTestApp2
+//
+//  Created by Kazuya Ito on 2020/03/24.
+//  Copyright © 2020 hoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
